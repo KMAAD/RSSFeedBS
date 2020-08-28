f6f3f6c54ab869fda832cca8524a43e99eadf1dd
kilianmadeira@gmail.com <kilianmadiera@gmail.com>
2020-08-28 15:06:03 -0500

    Added feed model

---
+++ feed/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Create your models here.
+class Feed(models.Model):
+    title = models.CharField(max_length=20)
+    link = models.URLField(max_length=200)
+    userId = models.IntegerField(max_length=11)
